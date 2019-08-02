5f1a88eb3ca7b474966c4a6c3905cdb06c5d266d
hian0204 <hiansilva0204@gmail.com>
2019-08-02 17:41:39 -0300

    parametros

---
+++ script.js
@@ -0,0 +1,71 @@
+//let lista = document.querySelector("ol");
+//let corpo = document.querySelector("body");
+
+//let raizes = [
+  //  {nome:"Coentro"},
+    //{nome:"Cebola"},
+    //{nome:"Rabanete"}
+//]
+
+//let imprimirComidas = (cor) =>{
+  //  for (let raiz of raizes){
+    //    raizes.innerHTML += `<li>${raiz.nome}</li>`
+    //}
+    //corpo.style.backgroundColor = cor;
+//}
+
+//let adcionarComida = (callback) =>{
+  //  setTimeout(() => {
+   // raizes.push({nome: "Gengibre"});
+   // callback("red");
+    //},2000);
+//}
+
+//adcionarComida(imprimirComidas);
+
+//let diasemana = "sextou";
+//let callback = (resolve, reject) =>{
+//    if(diasemana == "sextou"){
+//        resolve("Acertou")
+  //  } 
+  //  else{
+  //      reject("errou");
+  //  }
+//}
+
+//let promessa = new Promise(callback);
+
+
+
+//promessa
+  //  .then(console.log)
+  //  .catch(console.error)
+  //  .finally(console.log)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
